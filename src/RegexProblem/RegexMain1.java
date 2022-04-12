1afa24bf2b4efb1b4a6fa70748ea433cc1fa619d
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-12 16:49:46 +0530

    Validename

---
+++ src/RegexProblem/RegexMain1.java
@@ -0,0 +1,10 @@
+package RegexProblem;
+
+public class RegexMain1 {
+	public static void main(String[]args) {
+		RegexMethods rm=new RegexMethods();
+		System.out.println("ValidName");
+		rm.validname();
+
+}
+}
